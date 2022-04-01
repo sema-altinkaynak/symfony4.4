ed77616f79ce7093009ec5a10c85038849cc0cce
Semae Altinkaynak <saltinkaynak@meo-fichaux.fr>
2022-04-01 12:22:30 +0200

    Test unitaire

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -17,7 +17,7 @@ class HelloController extends AbstractController
     }
 
     #[Route('/hello/{name}/{times}', name: 'manytimes',requirements: ["times"=>"\d+"])]
-    public function helloManyTimes(string $name, int $times=3) : Response
+    public function helloManyTimes(string $name, int $times=2) : Response
     {
         return $this->render('hello/hellomanytimes.html.twig',array(
             "name"=>$name,
---
+++ tests/Controller/HelloControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Controller\HelloController;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HelloControllerTest extends WebTestCase
+{
+
+    public function testHelloManyTimes()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/hello/bob');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertCount(3, $client->getCrawler()->filter('p'));
+    }
+}
